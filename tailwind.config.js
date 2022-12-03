100bc232434cab45893f61110f035eacae597dbb
Arturo Salazar <arturosi1@csu.fullerton.edu>
2022-12-02 18:57:21 -0800

    Added tailwindcss
    Added a bunch of tailwindcss to make the entire site look a lot better looking

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
+    content: ['./*html'],
     theme: {
       extend: {}
     },
-    plugins: [
-      require('tw-elements/dist/plugin')
-    ]
+    plugins: []
   }
